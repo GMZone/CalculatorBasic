ba1798f8d199d403c0d2b6a265e1e699405ebf3e
unknown <gurmaymalvai16@gmail.com>
2020-12-13 18:21:41 +0530

    Finalized the calculator

--- Calculator.py
+++ Calculator.py
@@ -28,14 +28,23 @@ def TakeInput_Int():
 
 def TakeInput_Str():
     while True:
-        userin = input()
-        if userin == "":
-            continue
+        userin=input().lower()
+        if userin=="add" or userin=="subtract" or userin=="multiply" or userin=="divide":
+            return userin
         else:
-            if userin == "add" or userin == "subtract" or userin =="multiply" or userin == "divide":
-                break
-            else:
-                continue
+            continue
+
+def GoAgain():
+    userin=""
+    while True:
+        try:
+            userin=input("Type a yes or no").lower()
+        except:
+            print("")   
+        if userin=="yes" or userin=="no":
+            return userin
+        else:
+            continue     
 
 while True:
     print("Enter the first number")
@@ -45,4 +54,19 @@ while True:
     print("Would you like to:-\n1. Add\n2.Subtract\n3.Multiply\n4.Divide\n\n")
     operation = TakeInput_Str()
 
-    print(operation)
+    if operation=="add":
+        print(addition(num1, num2))
+    elif operation == "subtract":
+        print(subtraction(num1, num2))
+    elif operation == "multiply":
+        print(multiplication(num1, num2))
+    elif operation == "divide":
+        print(division(num1, num2))
+
+    print("Do you want to calculate again?\n")
+    goagain_decision=GoAgain()
+
+    if goagain_decision=="yes":
+        continue
+    elif goagain_decision=="no":
+        break
